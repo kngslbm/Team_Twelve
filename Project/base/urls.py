a85b96e84513635ae64cba3d57cb396c487f4f57
kngslbm <kangseulbeom@gmail.com>
2024-04-04 15:23:23 +0900

    backend 1차 완성

--- Project/base/urls.py
+++ Project/base/urls.py
@@ -3,6 +3,16 @@ from . import views
 
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
+
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
+
+    path('create_room/', views.createRoom, name="create-room"),
+    path('update_room/<str:pk>', views.updateRoom, name="update-room"),
+    path('delete_room/<str:pk>', views.deleteRoom, name="delete-room"),
+    path('delete_message/<str:pk>', views.deleteMessage, name="delete-message"),
 ]
